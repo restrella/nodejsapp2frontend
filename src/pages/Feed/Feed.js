8b87d70c869efaae7e5c498489e0a95f142aaa5c
learndev77 <restrella@77global.biz>
2024-07-04 10:52:48 +0800

    add rejectUnauthorized

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -36,11 +36,14 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket("http://localhost:8080");
+    // const socket = openSocket("http://localhost:8080"); // {rejectUnauthorized: false}
+    const socket = openSocket("http://localhost:8080", {rejectUnauthorized: false}); // {rejectUnauthorized: false}
     socket.on("posts", (data) => {
       console.log("posts", data);
       if (data.action === "create") {
         this.addPost(data.post);
+      } else if (data.action === "update") {
+        this.updatePost(data.post);
       }
     });
   }
@@ -57,6 +60,21 @@ class Feed extends Component {
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
       };
     });
   };
@@ -172,15 +190,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt,
         };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
